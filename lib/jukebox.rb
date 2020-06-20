ed60b3d10a0e73bf26c009a3945bc01c87e82eac
Thomas Bachorz <tjbachorz@gmail.com>
2020-06-20 02:29:40 +0000

    Automatically backed up by Learn

---
+++ lib/jukebox.rb
@@ -0,0 +1,28 @@
+require 'pry'
+
+songs = [
+  "Phoenix - 1901",
+  "Tokyo Police Club - Wait Up",
+  "Sufjan Stevens - Too Much",
+  "The Naked and the Famous - Young Blood",
+  "(Far From) Home - Tiga",
+  "The Cults - Abducted",
+  "Phoenix - Consolation Prizes",
+  "Harry Chapin - Cats in the Cradle",
+  "Amos Lee - Keep It Loose, Keep It Tight"
+]
+
+def help
+  puts "I accept the following commands:"
+  puts "- help : displays this help message"
+  puts "- list : displays a list of songs you can play"
+  puts "- play : lets you choose a song to play"
+  puts "- exit : exits this program"
+end
+
+def list(array)
+  index = 0 
+  while index < array.length do
+    puts "#{index + 1}. #{songs[index]}"
+  end
+end
